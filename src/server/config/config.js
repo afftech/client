2a6ee8135c8790e6fc8a6b6b344ea3ecb0341196
Андрей Рыжиков <andrei@nssz.local>
2021-03-02 17:33:13 +0300

    Ошибка
    TypeError: sequelize.import is not a function
        at C:\Users\andrei\Documents\client\src\server\models\index.js:19:35

---
+++ src/server/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  portl: 8081,
+  db:{
+    database: process.env.DB_NAME || 'users',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '995637',
+    options: {
+      dialect: process.env.DIALECT || 'mysql',
+      host: process.env.HOST || 'localhost'
+    }
+
+  }
+}
